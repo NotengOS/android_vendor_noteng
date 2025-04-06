33feb5fc46948ad0c0938a3babe94d752995c776
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-04-06 09:08:11 +0545

    config: Determine bootanimation resolution from TARGET_SCREEN_WIDTH
    Change-Id: Id3d4e226a62a6b672580463c7354c2f6e66aa651
    Signed-off-by: Hridaya Prajapati <info.hridayaprajapati@gmail.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,10 +1,10 @@
 # Bootanimation
-ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+ifeq ($(TARGET_SCREEN_WIDTH),1080)
 PRODUCT_COPY_FILES += vendor/noteng/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+else ifeq ($(TARGET_SCREEN_WIDTH),1440)
 PRODUCT_COPY_FILES += vendor/noteng/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+else ifeq ($(TARGET_SCREEN_WIDTH),720)
 PRODUCT_COPY_FILES += vendor/noteng/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else
-$(error Unsupported or undefined TARGET_BOOT_ANIMATION_RES: '$(TARGET_BOOT_ANIMATION_RES)')
+$(error Unsupported or undefined TARGET_SCREEN_WIDTH: '$(TARGET_SCREEN_WIDTH)')
 endif
