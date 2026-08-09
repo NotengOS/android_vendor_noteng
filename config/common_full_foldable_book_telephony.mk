pissel-os/android_vendor_custom

vendor: config: Inital basic common configs
Change-Id: I6d9fe2ad81d1a6451d532457763d01b14f8227e9

config/common_full_foldable_book_telephony.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_foldable_book_telephony.mk)
+
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common.mk)

config/common_full_phone.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common.mk)

config/common_full_tablet_wifionly.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common.mk)

config/common_full_tv.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tv.mk)
+
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common.mk)

config/common_mini_go_phone.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_mini_go_phone.mk)
+
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common.mk)

config/common_mini_phone.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_mini_phone.mk)
+
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common.mk)

config/common_mini_tablet_wifionly.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_mini_tablet_wifionly.mk)
+
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common.mk)

config/common_mini_tv.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_mini_tv.mk)
+
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common.mk)

NotengOS/android_vendor_noteng

config/common_full_foldable_book_telephony.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_foldable_book_telephony.mk)
+
+# Inherit some common NotengOS stuff.
+$(call inherit-product, vendor/noteng/config/common.mk)

config/common_full_phone.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit some common NotengOS stuff.
+$(call inherit-product, vendor/noteng/config/common.mk)

config/common_full_tablet_wifionly.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Inherit some common NotengOS stuff.
+$(call inherit-product, vendor/noteng/config/common.mk)

config/common_full_tv.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tv.mk)
+
+# Inherit some common NotengOS stuff.
+$(call inherit-product, vendor/noteng/config/common.mk)

config/common_mini_go_phone.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_mini_go_phone.mk)
+
+# Inherit some common NotengOS stuff.
+$(call inherit-product, vendor/noteng/config/common.mk)

config/common_mini_phone.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_mini_phone.mk)
+
+# Inherit some common NotengOS stuff.
+$(call inherit-product, vendor/noteng/config/common.mk)

config/common_mini_tablet_wifionly.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_mini_tablet_wifionly.mk)
+
+# Inherit some common NotengOS stuff.
+$(call inherit-product, vendor/noteng/config/common.mk)

config/common_mini_tv.mk
@@ -1,0 +1,5 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_mini_tv.mk)
+
+# Inherit some common NotengOS stuff.
+$(call inherit-product, vendor/noteng/config/common.mk)