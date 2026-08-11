33a86b1cf8cf4e3411fb90151167de8f4a6d6ad0
fiqys <elsifycritic@gmail.com>
2026-08-11 23:00:14 +0300

    vendor: Setup ntfonts repo
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- config/noteng.mk
+++ config/noteng.mk
@@ -4,6 +4,9 @@ $(call inherit-product, vendor/nothing/sounds/products/Sounds.mk)
 # Nothing Themepicker
 $(call inherit-product, vendor/nothing/themepicker/products/Themepicker.mk)
 
+# Nothing Fonts
+$(call inherit-product, vendor/nothing/ntfonts/products/NTFonts.mk)
+
 # Nothing Framework
 PRODUCT_SOONG_NAMESPACES += hardware/nothing
 $(call inherit-product, hardware/nothing/nt-fwk/nt-fwk.mk)
