6c93f2bbc8b4933c42c4a580938911fb5352dc6b
fiqys <elsifycritic@gmail.com>
2026-08-11 15:53:49 +0300

    vendor: Setup themepicker repo
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- config/noteng.mk
+++ config/noteng.mk
@@ -1,2 +1,5 @@
 # Nothing Sounds
 $(call inherit-product, vendor/nothing/sounds/products/Sounds.mk)
+
+# Nothing Themepicker
+$(call inherit-product, vendor/nothing/themepicker/products/Themepicker.mk)
