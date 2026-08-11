188daa9704565b25275064dab73f63241ab6325b
fiqys <elsifycritic@gmail.com>
2026-08-11 16:50:32 +0300

    vendor: Build Nothing Framework
    - Needed by the most apps Nothing ships
    
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- config/noteng.mk
+++ config/noteng.mk
@@ -3,3 +3,7 @@ $(call inherit-product, vendor/nothing/sounds/products/Sounds.mk)
 
 # Nothing Themepicker
 $(call inherit-product, vendor/nothing/themepicker/products/Themepicker.mk)
+
+# Nothing Framework
+PRODUCT_SOONG_NAMESPACES += hardware/nothing
+$(call inherit-product, hardware/nothing/nt-fwk/nt-fwk.mk)
